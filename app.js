cad03b989eb4451516457bd6bf75209803dfb703
Jon Preece <jonpreece@hotmail.co.uk>
2018-12-23 06:33:06 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+import Counter from "./counter.js";
+
+function App() {
+  return React.createElement(Counter, { count: 10 });
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,6 @@
+import App from "./app.js";
+
+ReactDOM.render(
+  React.createElement(App, null),
+  document.getElementById("root")
+);
